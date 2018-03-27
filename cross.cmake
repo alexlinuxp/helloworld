939cf81cb4baf4b55cb81759978fbd9d43751f77
alexlinuxp <37805099+alexlinuxp@users.noreply.github.com>
2018-03-28 01:55:10 +0200

    Update cross.cmake

--- cross.cmake
+++ cross.cmake
@@ -4,8 +4,8 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /home/Alex/Downloads/armv5-eabi--uclibc--stable-2018.02-2/bin/arm-buildroot-linux-uclibcgnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /home/Alex/Downloads/armv5-eabi--uclibc--stable-2018.02-2/bin/arm-buildroot-linux-uclibcgnueabi-g++)
+SET(CMAKE_C_COMPILER   ~/Downloads/armv5-eabi--uclibc--stable-2018.02-2/bin/arm-buildroot-linux-uclibcgnueabi-gcc)
+SET(CMAKE_CXX_COMPILER ~/Downloads/armv5-eabi--uclibc--stable-2018.02-2/bin/arm-buildroot-linux-uclibcgnueabi-g++)
 
 # where is the target environment 
 SET(CMAKE_FIND_ROOT_PATH  /home/Alex/Downloads/helloworld)
